e2abc8cc82913bd57652d3069037613d1dc842ee
啊!彬哥 <!@LAPTOP-LL0CEDGO>
2025-11-30 03:30:11 +0800

    加入專案檔案。

---
+++ Models/CalculatorModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmCalculator.Models
+{
+    public class CalculatorModel
+    {
+        public double Calculate(double a, double b, string op)
+        {
+            return op switch
+            {
+                "+" => a + b,
+                "-" => a - b,
+                "*" => a * b,
+                "/" => b == 0 ? 0 : a / b,
+                _ => 0
+            };
+        }
+    }
+}
---
+++ ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,79 @@
+﻿using MvvmCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace MvvmCalculator.ViewModels
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private readonly CalculatorModel _model = new CalculatorModel();
+
+        private string _display = "0";
+        private double _firstNumber = 0;
+        private string _operator = "";
+        private bool _isNewInput = true;
+
+        public string Display
+        {
+            get => _display;
+            set { _display = value; OnPropertyChanged(); }
+        }
+
+        public ICommand NumberCommand { get; }
+        public ICommand OperatorCommand { get; }
+        public ICommand EqualCommand { get; }
+        public ICommand ClearCommand { get; }
+
+        public CalculatorViewModel()
+        {
+            NumberCommand = new RelayCommand(param => AddNumber(param.ToString()));
+            OperatorCommand = new RelayCommand(param => SetOperator(param.ToString()));
+            EqualCommand = new RelayCommand(param => Calculate());
+            ClearCommand = new RelayCommand(param => Clear());
+        }
+
+        private void AddNumber(string num)
+        {
+            if (_isNewInput)
+            {
+                Display = num;
+                _isNewInput = false;
+            }
+            else
+            {
+                Display += num;
+            }
+        }
+
+        private void SetOperator(string op)
+        {
+            _firstNumber = double.Parse(Display);
+            _operator = op;
+            _isNewInput = true;
+        }
+
+        private void Calculate()
+        {
+            double second = double.Parse(Display);
+            double result = _model.Calculate(_firstNumber, second, _operator);
+            Display = result.ToString();
+            _isNewInput = true;
+        }
+
+        private void Clear()
+        {
+            Display = "0";
+            _firstNumber = 0;
+            _operator = "";
+            _isNewInput = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
